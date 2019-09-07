1e7960d6bcdbd9a872ea8764d40afe843186765a
Andrei Airinei <airinei.andrei@gmail.com>
2019-09-07 19:52:38 +0100

    Updated our application App component, sign-in component and sign-up component to use HOOKS

--- src/pages/collection/collection.component.jsx
+++ src/pages/collection/collection.component.jsx
@@ -1,9 +1,11 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { connect } from 'react-redux';
 
 import CollectionItem from '../../components/collection-item/collection-item.component';
 
 import { selectCollection } from '../../redux/shop/shop.selectors';
+
+import { firestore } from '../../firebase/firebase.utils';
 
 import {
   CollectionPageContainer,
@@ -12,6 +14,23 @@ import {
 } from './collection.styles';
 
 const CollectionPage = ({ collection }) => {
+  // ################## HOOKS TESTS ONLY ###############
+  // ################## FROM HERE ######################
+  useEffect(() => {
+    console.log('I am subscribing');
+    const unsubscribeFromCollections = firestore
+      .collection('collections')
+      .onSnapshot(snapshot => console.log(snapshot));
+
+    // replicating componentWillUnmount
+    // clean up function
+    return () => {
+      console.log('I am unsubscribing');
+      unsubscribeFromCollections();
+    };
+  }, []);
+  // ################## TO HERE ######################
+
   const { title, items } = collection;
   return (
     <CollectionPageContainer>
--- src/pages/shop/shop.component.jsx
+++ src/pages/shop/shop.component.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { Route } from 'react-router-dom';
 import { connect } from 'react-redux';
 
@@ -7,31 +7,31 @@ import { fetchCollectionsStart } from '../../redux/shop/shop.actions';
 import CollectionsOverviewContainer from '../../components/collections-overview/collections-overview.container';
 import CollectionPageContainer from '../collection/collection.container';
 
-class ShopPage extends React.Component {
-  componentDidMount() {
-    const { fetchCollectionsStart } = this.props;
-
+const ShopPage = ({ match, fetchCollectionsStart }) => {
+  useEffect(() => {
     fetchCollectionsStart();
-  }
+    // If we do not pass [], component will render twice
+    // First time when mounting the shop which triggers our useEffect to call for fetchCollectionsStart()
+    // Second time is after when our check current user session gets fired, gecause currentUser fro App changes
+    // It occurs when the user is logged in and we refresh the shop page
 
-  render() {
-    const { match } = this.props;
+    // Again, we pass the fetchCollectionsStart into array because of -> SEE APP.JS COMMENT <-
+  }, [fetchCollectionsStart]);
 
-    return (
-      <div className='shop-page'>
-        <Route
-          exact
-          path={`${match.path}`}
-          component={CollectionsOverviewContainer}
-        />
-        <Route
-          path={`${match.path}/:collectionId`}
-          component={CollectionPageContainer}
-        />
-      </div>
-    );
-  }
-}
+  return (
+    <div className='shop-page'>
+      <Route
+        exact
+        path={`${match.path}`}
+        component={CollectionsOverviewContainer}
+      />
+      <Route
+        path={`${match.path}/:collectionId`}
+        component={CollectionPageContainer}
+      />
+    </div>
+  );
+};
 
 const mapDispatchToProps = dispatch => ({
   fetchCollectionsStart: () => dispatch(fetchCollectionsStart())
